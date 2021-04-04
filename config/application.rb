5b118c93303e307e31e8988f905993a100d46a8e
SahilZ <sahil.maharjan@kotuko.it>
2021-04-04 13:59:05 +0545

    fix for deploy

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module CryptoPortfolio
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
+    config.serve_static_assets = true
     config.generators.system_tests = nil
   end
 end
