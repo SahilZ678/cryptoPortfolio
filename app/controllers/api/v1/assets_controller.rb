92a46e5c1c1c0c6c26563126198cde223c127511
SahilZ <sahil.maharjan@kotuko.it>
2021-04-09 22:18:19 +0545

    removed unwanted cod3e

--- app/controllers/api/v1/assets_controller.rb
+++ app/controllers/api/v1/assets_controller.rb
@@ -8,18 +8,13 @@ class Api::V1::AssetsController < ApplicationController
   end
 
   def show
-    if authorized?
       respond_to do |format|
         format.json { render :show }
       end
-    else
-      handle_unauthorized
-    end
   end
 
   def create
     @asset = current_user.assets.build(asset_params)
-    if authorized?
       respond_to do |format|
         if @asset.save
           format.json { render :show, status: :created, location: api_v1_asset_path(@asset) }
@@ -27,13 +22,9 @@ class Api::V1::AssetsController < ApplicationController
           format.json { render json: @asset.errors, status: :unprocessable_entity }
         end
       end
-    else
-      handle_unauthorized
-    end
   end
 
   def update
-    if authorized?
       respond_to do |format|
         if @asset.update(asset_params)
           format.json { render :show, status: :ok, location: api_v1_asset_path(@asset) }
@@ -41,38 +32,18 @@ class Api::V1::AssetsController < ApplicationController
           format.json { render json: @asset.errors, status: :unprocessable_entity }
         end
       end
-    else
-      handle_unauthorized
-    end
   end
 
   def destroy
-    if authorized?
       @asset.destroy
       respond_to do |format|
         format.json { head :no_content }
       end
-    else
-      handle_unauthorized
-    end
-
   end
 
   private
     def set_asset
       @asset = Asset.find(params[:id])
-    end
-
-    def authorized?
-      @asset.user == current_user
-    end
-
-    def handle_unauthorized
-      unless authorized?
-        respond_to do |format|
-          format.json { render :unauthorized, status: 401 }
-        end
-      end
     end
 
   def asset_params
